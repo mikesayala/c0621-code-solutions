f1754c228f88219177a9838aabe03c4360e99de2
Mike Ayala <ayala.s.mike@gmail.com>
2021-06-28 10:32:46 -0700

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Mike Ayala';
+var isCool = false;
+var totalPets = 0;
+
+console.log('value of FullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never', never);
+console.log('typeof never:', typeof never);
