94abcfe78c40522bb0b0fdfcb779f04e8a36d23f
Mike Ayala <ayala.s.mike@gmail.com>
2021-06-29 17:34:35 -0700

    completed string-manipulation-1

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,7 +1,8 @@
 /* exported reverseWord */
-// function reverseWord(word) {
-//   var str = '';
-//   for (var i = word.length - 1; i >= 0; i--) {
-//     retu
-//   }
-// }
+function reverseWord(word) {
+  var str = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    str += word[i];
+  }
+  return str;
+}
