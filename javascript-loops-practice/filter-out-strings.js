93dbc732f10a5e6287f914483462c77f3e962ac4
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-01 16:47:16 -0700

    changed condition from keeping numbers to filtering out strings

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   var arr = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       arr.push(values[i]);
     }
   }
