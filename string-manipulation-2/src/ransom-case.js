24f90b53fc9413db85f12ceaa00f5f8af8af18a0
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-01 14:34:06 -0700

    working on ransomCase function

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,8 @@
 /* exported ransomCase */
+function ransomCase(string) {
+  var str = '';
+  for (var i = 0; i < string.length; i++) {
+    str = string.substr(0, i) + string[i].toUpperCase() + string.substr(i + 1);
+  }
+  return str;
+}
