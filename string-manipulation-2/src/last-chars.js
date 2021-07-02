8822c27c2bfab29ccf21abd1c2e0bd1a7f1a639d
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-01 19:57:00 -0700

    working on it

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,8 @@
 /* exported lastChars */
+function lastChars(length, string) {
+  var word = '';
+  for (var i = 0; i < string.length; i++) {
+    word += string[i];
+  }
+  return word;
+}
--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,11 +1,11 @@
 /* exported numVowels */
 function numVowels(string) {
   var lowercase = string.toLowerCase();
-  var vowel = 'aeiou';
+  // var vowel = ['a', 'e', 'i', 'o', 'u'];
   var counter = 0;
   for (var i = 0; i < lowercase.length; i++) {
-    if (lowercase[i] === vowel[i]) {
-      counter++;
+    if (lowercase[i] === 'a' || lowercase[i] === 'e' || lowercase[i] === 'i' || lowercase[i] === 'o' || lowercase[i] === 'u') {
+      counter += 1;
     }
   }
   return counter;
