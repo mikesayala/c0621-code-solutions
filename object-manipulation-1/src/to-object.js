8d3345e88687e4432989e5fa7acc7520181e2c50
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-01 20:05:33 -0700

    fixed toObject

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,8 +1,6 @@
 /* exported toObject */
 function toObject(keyValuePair) {
   var obj = {};
-  for (var i = 0; i < keyValuePair.length - 1; i = i + 2) {
-    obj[keyValuePair[i]] = keyValuePair[i + 1];
-  }
+  obj[keyValuePair[0]] = keyValuePair[1];
   return obj;
 }
