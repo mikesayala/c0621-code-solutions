4cdcb3733978a28096028c76faa97595c3366b65
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-02 15:45:31 -0700

    changed the function to have arr[i] be in the condition

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -3,10 +3,9 @@ function compact(array) {
   var arr = array;
   var newArray = [];
   for (var i = 0; i < arr.length; i++) {
-    if (!arr[i]) {
-      continue;
+    if (arr[i]) {
+      newArray.push(arr[i]);
     }
-    newArray.push(arr[i]);
   }
   return newArray;
 }
