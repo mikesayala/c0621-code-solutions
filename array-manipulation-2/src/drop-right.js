233c82099084516a16147b99b58ddf6a57d318f2
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-06 11:15:07 -0700

    working on dropRight problem

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -1,4 +1,12 @@
 /* exported dropRight */
-// function dropRight(array, count) {
-
-// }
+function dropRight(array, count) {
+  var arr = [];
+  for (var i = count; i < array.length; i++) {
+    if (count > array.length) {
+      return array;
+    } else if (count <= array.length) {
+      arr.unshift(array[i]);
+    }
+  }
+  return arr;
+}
