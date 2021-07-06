de0e9678b591dfe58a5ce79fe0e34f53ef4d4030
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-05 21:45:25 -0700

    completed string-manipulation-2

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,6 +1,10 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
-  var newString = '';
-  newString = string.charAt(secondIndex) + string.substring(1, string.length - 1) + string.charAt(0);
-  return newString;
+  var first = firstIndex;
+  var second = secondIndex;
+  var stringSplit = string.split('');
+  [stringSplit[first], stringSplit[second]] = [stringSplit[second], stringSplit[first]];
+  var result = stringSplit.join('');
+  return result;
+
 }
