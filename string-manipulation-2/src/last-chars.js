7c428f7c0ae5dfdb0eebe07b48aa8122c791cd41
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-06 18:56:36 -0700

    fixed some code in lastChars function

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,8 +1,8 @@
 /* exported lastChars */
 function lastChars(length, string) {
-  var str = '';
+  var reverse = '';
   for (var i = string.length - 1; i >= 0; i--) {
-    var reverse = str += string[i];
+    reverse += string[i];
     var revert = reverse.substring(0, length);
     var result = revert.split('').reverse().join('');
   }
