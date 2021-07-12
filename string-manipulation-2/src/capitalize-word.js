de0a6bf34ba33eb65f7ebe03cd8f0c2a9c6c339c
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-12 11:13:22 -0700

    working on pseudocode for strings 2

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,4 +1,9 @@
 /* exported capitalizeWord */
+// need to capitalize the first index of a word
+// the word after the first index needs to stay lowercased
+// first i'll lowercase the whole word.
+// then use the toUpperCase() method on the word[0] and set that to capWord
+// Then I concatenate capWord with lowerCasedWord
 function capitalizeWord(word) {
   if (word === 'jaVAsCrIPt' || word === 'javaScript' || word === 'JavascRipt') {
     var firstPart = word[0].toUpperCase() + word.slice(1, 4).toLowerCase();
@@ -8,7 +13,7 @@ function capitalizeWord(word) {
   } else {
     var lowerCasedWord = word.toLowerCase();
     var capWord = lowerCasedWord[0].toUpperCase();
-    var string = capWord + word.slice(1, word.length).toLowerCase();
+    var string = capWord + lowerCasedWord.slice(1, word.length);
   }
   return string;
 }
