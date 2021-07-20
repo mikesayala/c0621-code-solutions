94123350c482d4f41f08478dbc2136158c5a9782
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-20 09:55:08 -0700

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -3,8 +3,20 @@
 // if the source.key is the same as the index in the array
 // maybe i need to use a for in loop since we're dealing with keys in objects
 // function pick(source, keys) {
-//   var result = {};
+//   var obj = {};
 //   for (var i = 0; i < keys.length; i++) {
-//     console.log(source[key]);
+//     if (source[keys[i]]) {
+//       obj[keys[i]] = source[keys[i]];
+//     }
 //   }
+//   return obj;
 // }
+function pick(source, keys) {
+  var newObject = {};
+  for (var i = 0; i < keys.length; i++) {
+    if (source[keys[i]]) {
+      newObject[keys[i]] = source[keys[i]];
+    }
+  }
+  return newObject;
+}
