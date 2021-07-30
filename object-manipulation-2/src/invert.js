b7e4bdc6b5b8bbea38cd90145c9f31cffa111baf
Mike Ayala <ayala.s.mike@gmail.com>
2021-07-29 22:11:34 -0700

    completed object-manipulation-2

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,9 @@
 /* exported invert */
+function invert(source) {
+  var object = {};
+  for (var key in source) {
+    var value = source[key];
+    object[value] = key;
+  }
+  return object;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -14,7 +14,7 @@
 function pick(source, keys) {
   var newObject = {};
   for (var i = 0; i < keys.length; i++) {
-    if (source[keys[i]]) {
+    if (source[keys[i]] !== undefined) {
       newObject[keys[i]] = source[keys[i]];
     }
   }
