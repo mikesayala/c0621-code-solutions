744995bd76e0dab73af6d2aeb37014a526e0d753
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-13 16:51:29 +0000

    finished isAnagram problem

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,12 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  const firstArray = firstString.split(' ');
+  const reunitedFirst = firstArray.join('');
+  const firstReunion = reunitedFirst.split('').sort().join('');
+
+  const secondArray = secondString.split(' ');
+  const reunitedSecond = secondArray.join('');
+  const secondReunion = reunitedSecond.split('').sort().join('');
+
+  return firstReunion === secondReunion;
+}
