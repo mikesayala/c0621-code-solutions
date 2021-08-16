28c30d3203593cbc9aa6ef0f53db7ee00027410d
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-16 04:05:32 +0000

    working on titleCase problem

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,10 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  let reversed = [];
+  const splitString = string.split(' ');
+  for (let i = 0; i < splitString.length; i++) {
+    reversed.push(splitString[i].split('').reverse().join(''));
+  }
+  reversed = reversed.join(' ');
+  return reversed;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,11 @@
 /* exported titleCase */
+function titleCase(title) {
+  var arr = title.split(' ');
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > 2 || arr[i] === arr[0] || arr === 'an' || arr !== 'to') {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    }
+  }
+  var test = arr.join(' ');
+  return test;
+}
