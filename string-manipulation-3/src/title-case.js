f70a449e02dcf8ccfe9092a0a99900f233fbffd7
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-16 21:51:31 +0000

    working on titleCase Still

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,7 +2,7 @@
 function titleCase(title) {
   var arr = title.split(' ');
   for (var i = 0; i < arr.length; i++) {
-    if (arr[i].length > 2 || arr[i] === arr[0] || arr === 'an' || arr !== 'to') {
+    if (arr[i].length > 2 || arr[i] === arr[0] || arr === 'an') {
       arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
     }
   }
