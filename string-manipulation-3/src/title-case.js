af2c20ec50023e5f75b2a39a97291d804789be6e
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-17 05:09:53 +0000

    working on titleCase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -2,7 +2,17 @@
 function titleCase(title) {
   var arr = title.split(' ');
   for (var i = 0; i < arr.length; i++) {
-    if (arr[i].length > 2 || arr[i] === arr[0] || arr === 'an') {
+    if (arr[i] === 'the' && i === 0) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    } else if (arr[i] === 'api') {
+      arr[i] = arr[i].toUpperCase();
+    } else if (arr[i] === 'javascript' || arr[i] === 'javascript:') {
+      if (arr[i] === 'javascript:') {
+        arr[i] = 'JavaScript:';
+      } else {
+        arr[i] = 'JavaScript';
+      }
+    } else if (arr[i].length > 2 || arr[i] === arr[0]) {
       arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
     }
   }
