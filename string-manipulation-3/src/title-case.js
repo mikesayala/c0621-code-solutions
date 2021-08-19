b5602bd69ee140cce338f507366d2bed95f351fc
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-19 04:44:09 +0000

    completed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,9 +1,22 @@
 /* exported titleCase */
 function titleCase(title) {
-  var arr = title.split(' ');
+  let arr = title.toLowerCase();
+  arr = arr.split(' ');
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === 'the' && i === 0) {
       arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    } else if (i !== 0 && arr[i - 1].includes(':')) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    } else if (arr[i] === 'the' && i !== 0) {
+      arr[i].toLowerCase();
+    } else if (arr[i] === 'for') {
+      arr[i].toLowerCase();
+    } else if (arr[i] === 'self-taught') {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1, 5) + arr[i][5].toUpperCase() + arr[i].slice(6);
+    } else if (i !== 0 && arr[i].includes('-')) {
+      if (arr[i][2] === '-') {
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1, 3) + arr[i][3].toUpperCase() + arr[i].slice(4);
+      }
     } else if (arr[i] === 'api') {
       arr[i] = arr[i].toUpperCase();
     } else if (arr[i] === 'javascript' || arr[i] === 'javascript:') {
@@ -16,6 +29,6 @@ function titleCase(title) {
       arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
     }
   }
-  var test = arr.join(' ');
+  const test = arr.join(' ');
   return test;
 }
