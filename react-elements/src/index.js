a713987f3a2abfc319c736db96ee8ee579b1786d
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-23 23:47:50 +0000

    completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const react = React.createElement('h1', null, 'Hello, React!');
+
+ReactDOM.render(react, document.getElementById('root'));
