98ade9e02e77e514b5c413695503b63fc094ce2b
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-27 23:29:32 +0000

    in progress of creating the accordion in my render method to see how it looks

---
+++ react-accordion-component/src/accordion.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Accordion extends React.Component {
+  render() {
+    return (
+     <div>
+       <div className="container">
+        <div className="box">
+          <h4>HyperText Markup Language</h4>
+        </div>
+       </div>
+     </div>
+    );
+  }
+}
+
+export default Accordion;
