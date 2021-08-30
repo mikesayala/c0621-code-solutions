e774bf41caebe095acb5d2d69670f740942ec55c
Mike Ayala <ayala.s.mike@gmail.com>
2021-08-30 18:04:30 +0000

    put variable absolutepath inside the express.static call and switched __dirname and 'public' in my join method call

--- express-static/index.js
+++ express-static/index.js
@@ -6,9 +6,9 @@ app.listen(3000, () => {
   console.log('Are ya ready kids? AYE AYE CAPTAIN');
 });
 
-const absolutePath = path.join('public', '__dirname');
+const absolutePath = path.join(__dirname, 'public');
 console.log(absolutePath);
 
-const staticExpress = express.static('public');
+const staticExpress = express.static(absolutePath);
 
 app.use(staticExpress);
